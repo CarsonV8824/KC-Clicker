80d1ecf9edd0b84f94ab9f8b6edfe1e2ae9bec8a
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-02-14 11:04:43 -0600

    added in more os fixes to files

--- main.py
+++ main.py
@@ -9,24 +9,23 @@ import sys, os
 def resource_path(relative_path):
     """Get absolute path to resource, works for dev and for PyInstaller"""
     if hasattr(sys, '_MEIPASS'):
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
-
-def get_image_path(image_name: str) -> str:
-    if image_name == "style.css":
-        return resource_path(os.path.join("styles", "style.css"))
-    return resource_path(os.path.join("images", image_name))
 
 def main() -> int:
     game_state = Database.load_game_state()
     app = QApplication(sys.argv)
     window = MainWindow(game_state)
-
-    with open(get_image_path("style.css"), "r") as f:
+    
+    css_path = resource_path(os.path.join("styles", "style.css"))
+    with open(css_path, "r") as f:
         app.setStyleSheet(f.read())
 
     app.setApplicationName("KC Clicker")
-    app.setWindowIcon(QIcon(get_image_path("dollar.png")))
+
+    icon_path = resource_path(os.path.join("images", "dollar.png"))
+    app.setWindowIcon(QIcon(icon_path))
 
     window.show()
     return app.exec(), game_state
